52ab4f2d17166ed090e15d64cc5f88e6ee03d5d2
민선 <seorivn@gmail.com>
2026-04-14 17:39:27 +0900

    [level 2] Title: 가장 큰 수, Time: 196.45 ms, Memory: 140 MB -BaekjoonHub

---
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+class Solution {
+    public String solution(int[] numbers) {
+        String[] arr = new String[numbers.length];
+        
+        for (int i = 0; i < numbers.length; i++) {
+            arr[i] = String.valueOf(numbers[i]);
+        }
+        
+        Arrays.sort(arr, (a,b) -> (b+a).compareTo(a+b));
+        
+        StringBuilder answer = new StringBuilder();
+        for (String s : arr) {
+            answer.append(s);
+        }
+        if (answer.charAt(0) == '0') return "0";
+        
+        return answer.toString();
+    }
+}
